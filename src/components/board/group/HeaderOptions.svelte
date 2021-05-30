a1d6683880262d9d458826693fbde92a9adada9b
Justin <faakjustin2@gmail.com>
2021-05-30 17:15:21 +0200

    added colors, group options menu

--- src/components/board/group/HeaderOptions.svelte
+++ src/components/board/group/HeaderOptions.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
+    import { fade } from "svelte/transition" 
     import { addItem } from "../../../utils/groupsData" 
 
     export let groups: any[]
     export let iGroups: number
+
+    let showMenu = false
 </script>
 
-<div>
-    <i class="options material-icons">more_horiz</i>
+<div class="more-wrapper">
+    <i on:click={e => showMenu = !showMenu} class="options material-icons">more_horiz</i>
+
+    <div class="more-menu-wrapper">
+        {#if showMenu}
+            <div class="invisible" on:click={() => showMenu = false}/>
+            <div class="more-menu" transition:fade={{ duration: 50 }}>
+                <p>Lorem Impsum</p>
+                <p>Lorem Impsum</p>
+                <p>Lorem Impsum</p>
+            </div>
+        {/if}
+    </div>
 </div>
+
 <div on:click={() => groups[iGroups].items = addItem(groups, iGroups)}>
     <i class="options material-icons">add</i>
 </div>
@@ -24,6 +39,41 @@
 
         &:hover {
             background: rgba(55, 53, 47, 0.08);
+        }
+    }
+
+    .more-wrapper {
+        position: relative;
+
+        .more-menu-wrapper {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 1%;
+
+            .invisible {
+                position: fixed;
+                top: 0;
+                left: 0;
+                z-index: 100;
+                width: 100vw;
+                height: 100vw;
+                background: transparent;
+            }
+
+            .more-menu {
+                position: absolute;
+                left: 50%;
+                transform: translateX(-50%);
+                width: 180px;
+                background: white;
+                box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
+                border-radius: 3px;
+                display: flex;
+                flex-direction: column;
+                z-index: 500;
+            }
         }
     }
 </style>
--- src/utils/randomColor.ts
+++ src/utils/randomColor.ts
@@ -1,6 +1,11 @@
 const colors = [
         "CBE4F9",
         "F9D8D6",
+        "daf9d6",
+        "f9f2d6",
+        "e0d6f9",
+        "f9d6ee",
+        "e9e9e9"
     ]
 
 export const randomColor = () => {
